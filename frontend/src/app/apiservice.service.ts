0a79e28fe734e19903b8b849aba9577f515667fd
Gustavo Angel <al221810743@gmail.com>
2021-11-16 23:07:31 -0600

    Inicio crud lista usuarios

---
+++ frontend/src/app/apiservice.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiserviceService {
+
+  constructor(private _http:HttpClient) { }
+  // Connect fronted to backend
+
+  apiUrl = 'http://localhost:3000/usuarios/';
+
+  //get all data
+  getAllData():Observable<any>
+  {
+    return this._http.get(`${this.apiUrl}`);
+  }
+}
